bb3f9e5c8aa971728285076d06de8a1e84746c3e
DESKTOP-B8DD8TC\hhw95 <hhw9560@naver.com>
2022-11-09 19:51:41 +0900

    - python 폴더 추가   - chromedriver.exe 추가   - lyric_crawling.py 추가 - server단에 http, cors 모듈 설치 - server.js /hello api 추가

---
+++ python/lyric_crawling.py
@@ -0,0 +1,62 @@
+# -*-coding:utf-8-*-
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
+
+# testURL = "https://naver.me/GLA7izLh"
+
+# f = open("test.txt", "w+")
+# f.write(testURL)
+# f.close()
+
+#f = open("test.txt", "r")
+#testString = f.readline().strip().strip("\"")
+# f.close()
+
+# print(testString)
+
+
+songTitle = sys.argv[1]
+
+url = "https://music.bugs.co.kr/search/integrated?q=" + songTitle
+driver = webdriver.Chrome()
+driver.get(url)
+driver.find_element(By.CLASS_NAME, "list.trackList").find_element(
+    By.TAG_NAME, "tbody").find_element(By.CLASS_NAME, "trackInfo").click()
+time.sleep(0.5)
+
+try:
+    # urllib.request.urlopen(driver.current_url)
+
+    #response = requests.get(driver.current_url)
+
+    # print(response)
+    # print(response.status_code)
+    # print(response.text)
+
+    #html = response.text
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    #imgSrc = soup.find('meta', {'property': 'og:image'}).get('content')
+    lyricContainer = soup.find('div', class_='lyricsContainer')
+    lyric = lyricContainer.find('xmp')
+
+    print(lyric.getText().strip())
+
+    #lambda lyric : lyric
+except IOError:
+    print("URL 주소가 올바르지 않습니다.")
+except:
+    print("오류가 발생하였습니다. 다시 시도해주십시오.")
